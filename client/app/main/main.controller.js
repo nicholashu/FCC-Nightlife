6dba77e6630957ed3b9ce308b2732f27d4cb5e2e
Nick Hu <nick@nickhu.info>
2015-10-01 12:15:14 +0800

    http get test

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -9,6 +9,11 @@ angular.module('nightlifeApp')
       socket.syncUpdates('thing', $scope.awesomeThings);
     });
 
+    $http.get("http://www.google.com/index.html", function(res) {
+      console.log("Got response: " + res.statusCode);
+    }).on('error', function(e) {
+      console.log("Got error: " + e.message);
+    });
     $scope.addThing = function() {
       if($scope.newThing === '') {
         return;
